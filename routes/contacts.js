c5242bf26a3c979aea095883fe04e7b80d71809b
Diegobr21 <jdiegobrana@gmail.com>
2021-11-07 00:41:33 -0600

    Adding contacts to logged in user

--- routes/contacts.js
+++ routes/contacts.js
@@ -27,8 +27,36 @@ router.get('/', auth, async (req, res) => {
 // @route   POST api/contacts
 // @desc    Add new contact
 // @access  Private
-router.post('/', (req, res) => {
-    res.send('Add new contact');
+router.post('/', [auth, [
+    check('email', 'Please enter a valid email').isEmail(),
+    check('name', 'Please enter a name').not().isEmpty(),
+
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ erros:errors.array() })
+    }
+
+    const { name, email, phone, type} = req.body;
+
+    try {
+        const newContact = new Contact({
+            name, 
+            email,
+            phone, 
+            type,
+            user: req.user.id
+        });
+
+        const contact = await newContact.save();
+
+        res.json(contact);
+
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 // @route   PUT api/contacts/:id
