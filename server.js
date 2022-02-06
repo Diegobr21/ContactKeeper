1802c9a1c767ec047fd6132d5e6f61bd2377670a
Diegobr21 <jdiegobrana@gmail.com>
2022-02-06 14:38:42 -0600

    build config set

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express();
 
@@ -9,7 +10,7 @@ connectDB();
 //Init Middleware
 app.use(express.json({extended: false}));
 
-app.get('/', (req, res) => res.json({msg: 'Welcome to ContactKeeperAPI'}));
+//app.get('/', (req, res) => res.json({msg: 'Welcome to ContactKeeperAPI'}));
 
 //Define routes
 
@@ -17,6 +18,14 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 
+    'index.html')));
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Listening in port ${PORT}`));
